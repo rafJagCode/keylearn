9d514c689fa78e6c8b7c25fd9ee57b0a841795f8
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2020-10-22 15:45:37 +0200

    Added vuetify

--- resources/components/Test.vue
+++ resources/components/Test.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Test</h1>
+  <v-app>
+    <v-alert type="warning" :value="true">
+      Vuetify was installed properly
+    </v-alert>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 window.Vue = require('vue');
+import vuetify from '@/plugins/vuetify';
 
-Vue.component('test', require('../components/test').default);
+Vue.component('test', require('@/components/test').default);
 
 const app = new Vue({
+    vuetify,
     el: '#app',
 });
