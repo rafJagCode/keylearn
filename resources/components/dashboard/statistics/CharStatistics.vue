e4cda797215c05763fbca19ccecb467b728bbe6f
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2021-04-03 22:33:22 +0200

    Added chars statistics

---
+++ resources/components/dashboard/statistics/CharStatistics.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-card class="test-results pa-2" width="250px" height="250px">
+		<v-layout column align-center fill-height justify-space-around>
+			<v-row>
+				<v-card-title class="text-heading-6 font-weight-black"> KEY: {{ charStatistics.char }} </v-card-title>
+			</v-row>
+			<v-card-text>
+				<v-row>
+					<v-col cols="auto">
+						<v-progress-circular :size="80" :width="8" :value="percentCorrect" color="teal" class="text-caption">
+							{{ percentCorrect }}%
+						</v-progress-circular>
+					</v-col>
+					<v-col cols="auto">
+						<v-progress-circular :size="80" :width="8" :value="percentSpeed" color="teal" class="text-caption">
+							{{ charStatistics.avgWpm }}WPM
+						</v-progress-circular>
+					</v-col>
+				</v-row>
+				<v-row class="text-subtitle-2 font-weight-black">
+					<v-col cols="auto">Accuracy</v-col>
+					<v-col cols="auto">Average speed</v-col>
+				</v-row>
+			</v-card-text>
+		</v-layout>
+	</v-card>
+</template>
+<script>
+export default {
+	props: {
+		charStatistics: null,
+		bestSpeed: null,
+	},
+	computed: {
+		percentCorrect() {
+			let all = this.charStatistics.all;
+			let incorrect = this.charStatistics.incorrect;
+			return Math.round(((all - incorrect) / all) * 100);
+		},
+		percentSpeed() {
+			let avgWpm = this.charStatistics.avgWpm;
+			return Math.round((avgWpm / this.bestSpeed) * 100);
+		},
+	},
+};
+</script>
--- resources/components/dashboard/statistics/CharsStatistics.vue
+++ resources/components/dashboard/statistics/CharsStatistics.vue
@@ -1,12 +1,17 @@
 <template>
 	<v-container class="chars-statistics">
-		<div v-for="charStatistics in charsStatistics" :key="charStatistics.id">
-			{{ charStatistics.char }}
-		</div>
+		<v-row>
+			<v-col class="my-2" cols="3" v-for="charStatistics in charsSegregated" :key="charStatistics.char">
+				<v-row justify="center">
+					<char-statistics :charStatistics="charStatistics" :bestSpeed="bestSpeed"></char-statistics>
+				</v-row>
+			</v-col>
+		</v-row>
 	</v-container>
 </template>
 <script>
 import { mapGetters } from 'vuex';
+import CharStatistics from '@/components/dashboard/statistics/CharStatistics.vue';
 export default {
 	computed: {
 		...mapGetters(['tests']),
@@ -17,6 +22,50 @@ export default {
 				})
 				.flat(Infinity);
 		},
+		charsSegregated() {
+			let chars = this.charsStatistics;
+			let charsSegregated = new Object();
+			chars.forEach((char) => {
+				if (char.char in charsSegregated) {
+					if (!char.correct) {
+						charsSegregated[char.char].incorrect++;
+					}
+					if (char.correct) {
+						charsSegregated[char.char].totalTime += char.time;
+					}
+					charsSegregated[char.char].all++;
+				} else {
+					let newChar = {
+						[char.char]: {
+							incorrect: char.correct ? 0 : 1,
+							all: 1,
+							totalTime: char.correct ? char.time : 0,
+						},
+					};
+					charsSegregated = { ...charsSegregated, ...newChar };
+				}
+			});
+			let processed = Object.entries(charsSegregated).map(([key, value]) => {
+				return {
+					char: key,
+					all: value.all,
+					incorrect: value.incorrect,
+					avgWpm: Math.round(60 / (value.totalTime / (value.all - value.incorrect)) / 5),
+				};
+			});
+			return processed.filter((char) => {
+				return char.all - char.incorrect;
+			});
+		},
+		bestSpeed() {
+			let chars = this.charsSegregated;
+			let maxWpm = chars[0].avgWpm;
+			chars.forEach((char) => {
+				if (char.avgWpm > maxWpm) maxWpm = char.avgWpm;
+			});
+			return maxWpm;
+		},
 	},
+	components: { CharStatistics },
 };
 </script>
