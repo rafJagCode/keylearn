dfe5c9f95aca0bf930f9b8515e94bb6e9858d019
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-05-28 01:12:44 +0200

    Fixed displayed error on login with incorrect credentials

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,10 +21,8 @@ class LoginController extends Controller
       if (Auth::attempt($request->only('email', 'password'))) {
         return response()->json(Auth::user(), 200);
       } else {
-        throw new IncorrectLoginCredentials('Provided credentials are incorrect');
+        return response()->json(['message' => 'Provided credentials were incorrect'], 401);
       }
-    } catch (IncorrectLoginCredentials $e) {
-      throw $e;
     } catch (QueryException $e) {
       throw new Exception('Server is having trouble right now. Please try again later', $e->getCode(), $e);
     } catch (Exception $e) {
